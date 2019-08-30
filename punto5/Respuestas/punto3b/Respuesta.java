cc8ade73843909f4c9c2dd4d9ff9f5f8d5e33047
anele <anelegaribaldi@gmail.com>
2019-08-30 19:47:13 -0300

    cambios

--- punto5/Respuestas/punto3b/Respuesta.java
+++
@@ -1,7 +0,0 @@
-package punto3b;
-
-public class Respuesta implements java.io.Serializable {
-
-	private static final long serialVersionUID = 1L;
-
-}
--- punto5/src/punto3b/Irfs.java
+++
@@ -1,9 +0,0 @@
-package punto3b;
-
-public interface Irfs {
-	/* Interfaz que vamos a utilizar declarando las cabeceras de los metodos solamente. similar a RFS.X en rpcgen  */
-	float abrir(float numero1, float numero2);
-	float leer(float numero1, float numero2);
-	float escribir(float numero1, float numero2);
-	float cerrar(float numero1, float numero2);
-}
